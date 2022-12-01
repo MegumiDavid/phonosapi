ef3c2b008e91afe39df4fe281012a42b1c14c211
David Megumi <dvdshinjo@gmail.com>
2022-11-30 22:52:08 -0300

    controller pacientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PacientesController } from './pacientes/pacientes.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PacientesController],
   providers: [AppService],
 })
 export class AppModule {}
